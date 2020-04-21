17394006744b5e5716d39d5319df0e306d1626bb
Alexandru <rammazzoti2000@gmail.com>
2020-04-21 15:29:52 +0200

    create html form to create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(users_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def users_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create]
 end
