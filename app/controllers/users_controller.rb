a152d99eb93a7c13d0c7e48ee19f796e4478e3f3
vzdrizhni <vzdrizhni@gmail.com>
2020-04-21 20:20:49 +0500

    added edit and extra credit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(users_params)
+    @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
@@ -12,8 +12,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
+  end
+
   private
-  def users_params
+  def user_params
     params.require(:user).permit(:username, :email, :password)
   end
 end
